f92326b8399e205f087d5ec2863069e8783e6572
MSantos015 <lolnaticos.com@gmail.com>
2017-11-29 11:36:40 -0200

    Comecando a implementar um metodo que imprime o tabuleiro

--- TrabalhoFinal-Marcelo/Program.cs
+++ TrabalhoFinal-Marcelo/Program.cs
@@ -7,6 +7,9 @@ namespace TrabalhoFinal_Marcelo
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
