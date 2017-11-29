651d04c275b0cd2deec1904ff6ad2217694ba9cf
MSantos015 <lolnaticos.com@gmail.com>
2017-11-29 10:56:27 -0200

    Classe Posicao

--- TrabalhoFinal-Marcelo/Program.cs
+++ TrabalhoFinal-Marcelo/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Tabuleiro;
 namespace TrabalhoFinal_Marcelo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
         }
+        
     }
 }
