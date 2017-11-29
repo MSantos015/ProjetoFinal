5ad54026473d4682e2bd57e4aea4540ddeb11458
MSantos015 <lolnaticos.com@gmail.com>
2017-11-29 11:21:25 -0200

    Comecando a implementar o tabuleiro

--- TrabalhoFinal-Marcelo/Program.cs
+++ TrabalhoFinal-Marcelo/Program.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 namespace TrabalhoFinal_Marcelo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
-
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
-        
     }
 }
