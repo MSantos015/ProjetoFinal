05d03a5361edfb745a78d75d9a819e4b79df09e2
MSantos015 <lolnaticos.com@gmail.com>
2017-12-06 11:05:53 -0200

    Fazendo primeiros testes de movimentacao

--- TrabalhoFinal-Marcelo/Program.cs
+++ TrabalhoFinal-Marcelo/Program.cs
@@ -6,16 +6,24 @@ namespace TrabalhoFinal_Marcelo {
     class Program {
         static void Main(string[] args) {
             try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(6, 2));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 4));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2,6));
-                Tela.imprimirTabuleiro(tab);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada) {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executarMovimento(origem, destino);
+                }
+
+                Tela.imprimirTabuleiro(partida.tab);
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
