715867756a4d0cd78a852f459fde239103b8bec7
MSantos015 <zumiizombie@gmail.com>
2017-12-07 17:18:22 -0200

    Testes iniciais com movimentos possiveis das pecas

--- TrabalhoFinal-Marcelo/Tela.cs
+++ TrabalhoFinal-Marcelo/Tela.cs
@@ -2,32 +2,43 @@
 using tabuleiro;
 using xadrez;
 
-namespace TrabalhoFinal_Marcelo
-{
-    class Tela
-    {
-        public static void imprimirTabuleiro(Tabuleiro tab)
-        {
-            for (int i = 0; i < tab.linhas; i++)
-            {
+namespace TrabalhoFinal_Marcelo {
+    class Tela {
+        public static void imprimirTabuleiro(Tabuleiro tab) {
+            for (int i = 0; i < tab.linhas; i++) {
                 Console.Write(8 - i + " ");
-                for (int j = 0; j < tab.linhas; j++)
-                {
-                    if (tab.peca(i, j) == null)
-                    {
-                        Console.Write("- ");
-                    }
-                    else
-                    {
-                        Tela.imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-                    }
-                    
+                for (int j = 0; j < tab.linhas; j++) {
+                    imprimirPeca(tab.peca(i, j));
+
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  A B C D E F G H");
         }
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis) {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++) {
+                    if (posicoesPossiveis[i, j]) {
+                        Console.BackgroundColor = fundoAlterado;
+                    }
+                    else {
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+                    imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+            Console.BackgroundColor = fundoOriginal;
+
+        }
+
 
         public static PosicaoXadrez lerPosicaoXadrez() {
             string s = Console.ReadLine();
@@ -37,19 +48,25 @@ namespace TrabalhoFinal_Marcelo
         }
 
         public static void imprimirPeca(Peca peca) {
-            if (peca.cor == Cor.Branca)
-            {
-                Console.Write(peca);
+            if (peca == null) {
+                Console.Write("- ");
+            }
 
-            } 
-            else
-            {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+            else {
+
+                if (peca.cor == Cor.Branca) {
+                    Console.Write(peca);
+
+                }
+                else {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
-        
+
     }
 }
--- TrabalhoFinal-Marcelo/tabuleiro/Peca.cs
+++ TrabalhoFinal-Marcelo/tabuleiro/Peca.cs
@@ -1,17 +1,14 @@
 ﻿
 
-namespace tabuleiro
-{
-    class Peca
-    {
+namespace tabuleiro {
+    abstract class Peca {
 
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Tabuleiro tab, Cor cor)
-        {
+        public Peca(Tabuleiro tab, Cor cor) {
             this.posicao = null;
             this.tab = tab;
             this.cor = cor;
@@ -20,6 +17,9 @@ namespace tabuleiro
         public void incrementarQteMovimentos() {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+
 
     }
 }
